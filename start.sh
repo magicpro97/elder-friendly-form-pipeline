c265faefbe3e5bc00c08515b75cfa308e8491802
Linh Ngo <thlinh.ngo@gmail.com>
2025-10-31 18:49:09 +0700

    chore: Remove unused test and setup scripts

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Start script for Railway deployment
-# Ensures PORT variable is properly read and passed to uvicorn
-
-PORT=${PORT:-8000}
-echo "Starting uvicorn on port $PORT"
-exec uvicorn app:app --host 0.0.0.0 --port $PORT
